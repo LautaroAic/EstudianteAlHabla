d30fbad30fa00643ba81444cc8d3d596f11489e2
Lautaro Aichino <laichino@escuelasproa.edu.ar>
2023-06-27 20:06:52 -0300

    termine la estructura del buscador

--- java.js
+++ java.js
@@ -43,18 +43,58 @@ box_search =        document.getElementById("box-search");
 //Funcion para mostrar el buscador
 function mostrar_buscador(){
 
-    bars_search.style.top = "130px";
+    bars_search.style.top = "150px";
     cover_ctn_search.style.display = "block";
     inputSearch.focus();
+
+    if (inputSearch.value === ""){
+        box_search.style.display = "none";
+    }
 
 }
 
 //Funcion para ocultar el buscador
 function ocultar_buscador(){
 
-    bars_search.style.top = "-100px";
+    bars_search.style.top = "-90px";
     cover_ctn_search.style.display = "none";
     inputSearch.value = "";
-    
+    box_search.style.display = "none";
+
+}
+
+
+//Creando filtrado de busqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+
 
 }
