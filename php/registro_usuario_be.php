896a7fc5d4d816ff1bf6745f2d285229fbea5d5c
Nahiquiroga <laichino@escuelasproa.edu.ar>
2023-06-30 08:23:03 -0300

    termine el crud php y mysql

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -8,9 +8,21 @@
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $query =  "INSERT INTO usuarios(nombre_competo, Correo, usuario, contrasena) 
+    $query =  "INSERT INTO usuarios(nombre_competo, correo, usuario, contrasena) 
                 VALUES('$nombre_completo','$correo','$usuario','$contrasena')" ;
 
+    //Verificar que no se repita el correo en la Base de datos
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo='$correo'");
+    
+    if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este correo ya esta en uso");
+                window.location = "../Inicio_Sesion.php";
+            </script>
+        ';
+    }
+    
     $ejecutar = mysqli_query($conexion, $query);
 
     if($ejecutar){
@@ -20,5 +32,15 @@
                 window.location = "../Inicio_Sesion.php";
                 </script>
         ';
+    }else{
+         echo '
+            <script>
+                alert("Intentelo de nuevo, usuario no almacenado");
+                window.location = "../Inicio_Sesion.php";
+                </script>
+        ';
     }
+
+mysqli_close($conexion);
+
 ?>
